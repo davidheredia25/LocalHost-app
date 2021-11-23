8954319e6183eeda2e13c18f76b98403ad1a9f06
molucax <moraleslucasmanuel@gmail.com>
2021-11-23 14:30:32 -0300

    intitial files

---
+++ client/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import axios from 'axios';
+
+import dotenv from "dotenv";
+dotenv.config();
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
