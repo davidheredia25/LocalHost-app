0f68c362c3515d86d3fc6d35c06ce9052822a2d5
Tomas Pereletegui <tpereletegui@MacBook-Air-de-Tomas.local>
2021-11-26 07:54:54 -0300

    style

--- client/src/components/user/Card/Card.js
+++ client/src/components/user/Card/Card.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import './Card.scss';
+import { Button } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 
 const Card = ({product}) => {
@@ -9,19 +11,19 @@ const Card = ({product}) => {
     console.log('cardProduct',product)
       
     return (
-        <div className='container'>
+        <div className='container_card'>
                 
                 <div >
-                    <img className='image' src={product.image}></img>
+                    <img className='card_image' src={product.image}></img>
                 </div >
-                <div>
-                    <p className='name'>{product.name}</p>
-                </div >
-                    <div>
-                    <p className='name'>{product.price}</p>
+                <div className='card_div_name'>
+                    <p className='card_name'>{product.name}</p>
+                </div>
+                    <div className='card_div_name'>
+                    <p className='card_price'>${product.price}</p>
                     </div>
-            <div className='button'>
-                 <button>Add to Cart</button>
+            <div className='card_div_button'>
+                 <Button variant='contained' size="small" style={{'backgroundColor': '#000000', 'width': 70}}  ><ShoppingCartIcon/></Button>
                 </div> 
             
             
