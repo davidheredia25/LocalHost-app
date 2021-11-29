a19fa815ad2f1d5aaf5ec94de54a74138097746e
Samuel <samuelcantos18@gmail.com>
2021-11-29 05:13:58 -0300

    middleware

--- src/routes/Brand/Controllers/middleware.js
+++ src/routes/Brand/Controllers/middleware.js
@@ -7,7 +7,7 @@ const verificacionName = async (name) => {
         let find = await Brand.find({name: name});
         // console.log('find verificacionName', find);
         let obj = { bool: false };
-        if(find.lenght !== 0 || find !== null) return obj = { bool: true };
+        if(find.lenght !== 0) return obj = { bool: true };
         return obj;
     } catch (error) {
         console.log(error);
@@ -19,7 +19,8 @@ const verificacionId = async (id) => {
         let find = await Brand.findById(id);
         // console.log('find verificacionId', find);
         let obj = { bool: false };
-        if(find.length !== 0 || find !== null) return obj = { bool: true, brand: find };
+        let prop = Object.keys(find);
+        if(prop.length !== 0) return obj = { bool: true, brand: find };
         return obj;
     } catch (error) {
         console.log(error);
@@ -28,10 +29,11 @@ const verificacionId = async (id) => {
 
 const verificacionC = async (name) => {
     try {
-        let find = await Category.find({name: name}); 
+        let find = await Category.findOne({name: name}); 
         // console.log('find verificacionC', find);
         let obj = { bool: false };
-        if(find.length !== 0 || find !== null) return obj = { bool: true, category: find._id };
+        let prop = Object.keys(find);
+        if(prop.length !== 0) return obj = { bool: true, category: find._id };
         return obj;
     } catch (error) {
         console.log(error);
@@ -40,10 +42,11 @@ const verificacionC = async (name) => {
 
 const verificacionT = async (name) => {
     try {
-        let find = await Types.find({name: name}); 
+        let find = await Types.findOne({name: name}); 
         // console.log('find verificacionT', find);
         let obj = { bool: false };
-        if(find.length !== 0 || find !== null) return obj = { bool: true, type: find._id };
+        let prop = Object.keys(find);
+        if(prop.length !== 0) return obj = { bool: true, type: find._id };
         return obj;
     } catch (error) {
         console.log(error);
