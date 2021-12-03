c36310623ce2c53f2a6c53caabbe0bdcbbde089e
David Heredia <davis_heredia@hotmail.com>
2021-12-03 17:53:49 -0300

    ProfileArreglos

--- client/src/components/user/Profile/NavBarProfile.jsx
+++ client/src/components/user/Profile/NavBarProfile.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import style from './Styles/NavBarProfile.module.scss';
-import img from './Image.jpeg'
+import { useSelector } from 'react-redux';
+import { BsPersonCircle } from 'react-icons/bs';
 
 
 const NavBarProfile = () => {
+
+    const { user } = useSelector(state => state.login)
     return (
 
         <div className={style.ctnNavBar}>
-            <img className={style.imgUser} src={img} alt={img} />
-            <h1 className={style.name}>Hola! David Heredia</h1>
+            <div > <BsPersonCircle className={style.iconUser}  /> </div>
+
+            <h1 className={style.name}>Hola! {user?.user?.fristName} {user?.user?.lastName}</h1>
             <ul className={style.ctnUl}>
                 <Link className={style.link} to="/profile"><li><button className={style.btn}> Mis Datos</button> </li></Link>
                 <Link className={style.link} to="/profile/misordenes"><li><button className={style.btn}> Mis ordenes</button> </li> </Link>
