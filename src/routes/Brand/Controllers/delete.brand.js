a761947d9494e7036db9a968e49f932f752fdc62
Marco-Carballo-Francia <solojuegos.kiku2020@gmail.com>
2021-12-09 02:47:53 -0300

    Deleted terminado, no esta probado pero deberia de funcionar

--- src/routes/Brand/Controllers/delete.brand.js
+++ src/routes/Brand/Controllers/delete.brand.js
@@ -1,25 +1,38 @@
 const Brand = require('../../../models/Brand');
+const Product = require('../../../models/Product');
 const { verificacionId } = require('./middleware');
 
+// No lo quise probar a esta hora pq si me equiboco en algo no tengos los json para 
+// agregar los productos de nuevo
+
 const deleteBrand = async (req, res) => {
-    const { brand } = req.body;
     // Acá se eliminarían todos los productos de esa marca y también la marca del modelo Brand
-    
-    /*  const { id } = req.params;
-    // console.log('id deleteBrand', id);
+    const { id } = req.params; // Solo con esto puedo encontrar las brands de los productos
+    console.log('id deleteBrand', id);
     try {
         let verificacion = await verificacionId(id);
+        console.log('verificacion deleteBrand: ', verificacion);
 
-        if(verificacion.bool) {
-            let deleted = await Brand.findByIdAndDelete(id)
-            // console.log('deleted deleteBrand', deleted);
-            if(deleted)  return res.json(deleted);
+        if (verificacion.bool) {
+            let findProduct = await Product.find({ brand: verificacion.brand._id });
+            let arrayDelete = [];
+            if(findProduct.length !== 0) {
+                findProduct.forEach(async (p) => {
+                    console.log('p deleteBrand: ', p.name);
+                    let deletedProduct = await Product.findByIdAndDelete(p._id);
+                    console.log('deletedProduct deleteBrand: ', deletedProduct.name);
+                    arrayDelete.push(deletedProduct.name);
+                });
+            }
+            let deleted = await Brand.findByIdAndDelete(id);
+            console.log('deleted deleteBrand', deleted);
+            if (deleted) return res.json([deleted, ...arrayDelete]);
             return res.send('No se elimino correctamente');
         }
         res.send('No se encontro la marca');
     } catch (error) {
         console.log(error);
-    } */
+    }
 };
 
 module.exports = {
