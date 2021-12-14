cc9b5981bc371b02ad161a0b0e1b4e92a6988da0
Martin Morlio <tincho20012017@gmail.com>
2021-12-14 14:44:21 -0300

    credenciales actualizadas

--- src/routes/User/Controllers/nodemailer.user.js
+++ src/routes/User/Controllers/nodemailer.user.js
@@ -1,5 +1,5 @@
 const nodemailer = require("nodemailer");
-const {google} = require("googleapis");
+const { google } = require("googleapis");
 
 /* 
     Esta función lo único que hace es enviar un link al mail del user
@@ -7,66 +7,62 @@ const {google} = require("googleapis");
 */
 
 const enviarMail = async (req, res) => {
-    
   const { CLIENT_ID, REFRESH_TOKEN, CLIENT_SECRET, REDIRECT_URI } = process.env;
-  
-  const {email}  = req.body;
-  
+
+  const { email } = req.body;
+
   try {
-      //voy a recibir el email por body para verificar si existe user.
-   if(email){
-  
-    // const contentHTML = `
-    //             <h1>Formulario de nodemailer</h1>
-    //             <ul>
-    //                  </li>
-    //                 <li>Email: ${email} </li>
-    //                 <li>Password: ${password} </li>
-    //             </ul>`;
+    //voy a recibir el email por body para verificar si existe user.
+    if (email) {
+      const contentHTML = `
+                <h1>Recuperar contraseña</h1>
+                <h3> Para ingresar su nueva contraseña, entre al siguiente link
+                http://localhost:3000/user/login/password </h3>
+                `;
 
-    const link = `http://localhost:3000/user/login/password`;
+      // const link = `http://localhost:3000/user/login/password`;
 
-    const oAuth2Client = new google.auth.OAuth2(
-      CLIENT_ID,
-      CLIENT_SECRET,
-      REDIRECT_URI
-    );
-    oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+      const oAuth2Client = new google.auth.OAuth2(
+        CLIENT_ID,
+        CLIENT_SECRET,
+        REDIRECT_URI
+      );
+      oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
-    async function sendMail() {
-      try {
-        const accessToken = await oAuth2Client.getAccessToken();
-        const transporter = nodemailer.createTransport({
-          service: "gmail",
-          auth: {
-            type: "OAuth2",
-            user: "tincho20012017@gmail.com",
-            clientId: CLIENT_ID,
-            clientSecret: CLIENT_SECRET,
-            refreshToken: REFRESH_TOKEN,
-            accessToken: accessToken,
-          },
-        });
-        const mailOptions = {
-          from: "VSClothes <tincho20012017@gmail.com>",
-          to: email,
-          subject: "Recuperación de contraseña",
-          text: link,
-        };
+      async function sendMail() {
+        try {
+          const accessToken = await oAuth2Client.getAccessToken();
+          const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+              type: "OAuth2",
+              user: "vsclothes2@gmail.com",
+              clientId: CLIENT_ID,
+              clientSecret: CLIENT_SECRET,
+              refreshToken: REFRESH_TOKEN,
+              accessToken: accessToken,
+            },
+          });
+          const mailOptions = {
+            from: "VSClothes <vsclothes2@gmail.com>",
+            to: email,
+            subject: "Recuperación de contraseña",
+            html: contentHTML,
+          };
 
-        const result = await transporter.sendMail(mailOptions);
-        console.log(result);
-        res.status(200).json("Enviado")
-      } catch (error) {
-        console.log(error);
+          const result = await transporter.sendMail(mailOptions);
+          console.log(result);
+          res.status(200).json("Enviado");
+        } catch (error) {
+          console.log(error);
+        }
       }
-    }
 
-    sendMail()
-      .then((res) => {
-        res.status(200).send("enviado");
-      })
-      .catch((error) => console.log(error.message));
+      sendMail()
+        .then((res) => {
+          res.status(200).send("enviado");
+        })
+        .catch((error) => console.log(error.message));
     }
   } catch (err) {
     console.log(err);
